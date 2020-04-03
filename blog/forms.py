68fbdb70a31d25a5f33c451a9bee9e66b4461c33
ismaroma <ismaroma@gmail.com>
2020-04-03 09:19:27 +0200

    Modified

--- blog/forms.py
+++ blog/forms.py
@@ -7,10 +7,16 @@ Created on Wed Apr  1 14:22:46 2020
 
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)

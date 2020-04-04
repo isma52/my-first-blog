debc00bd63bb717e0fec8eb0dadbeda2aa0fd072
isma52 <62899742+isma52@users.noreply.github.com>
2020-04-04 19:19:57 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ismablog',
         'USER': 'isma2',
-        'PASSWORD': 'Tinpony70',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
